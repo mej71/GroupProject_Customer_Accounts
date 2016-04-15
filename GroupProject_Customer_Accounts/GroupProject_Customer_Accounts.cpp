14b69bf5f9809c5e40843442a6654c146c56538e
mej71 <mej7171@gmail.com>
2016-04-15 16:30:50 -0500

    Added temp variables and main controller

--- GroupProject_Customer_Accounts/GroupProject_Customer_Accounts.cpp
+++ GroupProject_Customer_Accounts/GroupProject_Customer_Accounts.cpp
@@ -4,8 +4,33 @@
 #include "stdafx.h"
 #include <iostream>
 
+class MenuController {
+
+	private:
+		int tempInt;
+		std::string tempStr;
+		bool tempBool;
+		
+
+	public:
+		MenuController() {
+			tempInt = 0;
+			tempStr = "";
+			tempBool = false;
+		}
+
+};
+
+
+
 int main()
 {
+	//declare local variables
+	std::string runAgain = "Y";
+	MenuController menuController;
+	while (runAgain == "Y") {
+
+	}
     return 0;
 }
 
