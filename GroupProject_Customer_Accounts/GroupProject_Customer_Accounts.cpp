0281e46d770fbd603729987f084d7ec5a11de78a
mej71 <mej7171@gmail.com>
2016-04-16 13:36:42 -0500

    Wrote the controller class (mostly), and redid the main method.

--- GroupProject_Customer_Accounts/GroupProject_Customer_Accounts.cpp
+++ GroupProject_Customer_Accounts/GroupProject_Customer_Accounts.cpp
@@ -3,34 +3,22 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "MenuController.h";
 
-class MenuController {
 
-	private:
-		int tempInt;
-		std::string tempStr;
-		bool tempBool;
-		
-
-	public:
-		MenuController() {
-			tempInt = 0;
-			tempStr = "";
-			tempBool = false;
-		}
-
-};
-
+void showWelcomeMessage() {
+	std::cout << "Welcome to the Customer Accounts Information program, developed by Jem Milam and Robert Munshower." << std::endl;
+}
 
 
 int main()
 {
-	//declare local variables
 	std::string runAgain = "Y";
-	MenuController menuController;
-	while (runAgain == "Y") {
-
-	}
+	MenuController *menuController;
+	showWelcomeMessage();
+	//start main controller, shows menu
+	menuController = new MenuController();
+	system("pause");
     return 0;
 }
 
